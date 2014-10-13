35c9bd59b9688b54ff876d2a86c1d2cc195f32cd
Holen Zhan <holen.zh@gmail.com>
2014-10-13 21:47:04 +0800

    完成baby表单页设计，Activity切换以及保存确认等功能

--- src/com/holen/babygrowth/MainActivity.java
+++ src/com/holen/babygrowth/MainActivity.java
@@ -3,15 +3,20 @@ package com.holen.babygrowth;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import android.os.Build;
 
 public class MainActivity extends Activity {
+	
+	private long exitTime = 0;                                          
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +27,12 @@ public class MainActivity extends Activity {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		
 	}
-
+	
+	/**
+	 * 添加菜单选项
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -31,14 +40,24 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+	/**
+	 * 菜单选项监听器
+	 */
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.main_addBaby) {
+			Intent intent = new Intent(MainActivity.this , BabyDataEdit.class);
+			startActivity(intent);
+			return true;
+		}else if(id == R.id.main_about){
+			return true;
+		}else if(id == R.id.main_exit){
+			finish();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
@@ -58,6 +77,32 @@ public class MainActivity extends Activity {
 			View rootView = inflater.inflate(R.layout.fragment_main, container,
 					false);
 			return rootView;
+		}
+	}
+	
+	/**
+	 * 捕捉返回事件按钮
+	 */
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+		if(keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN){
+			exitApp();
+			return true;
+		}else{
+			return super.onKeyDown(keyCode, event);
+		}
+	}
+	
+	/**
+	 * 退出程序逻辑
+	 */
+	public void exitApp(){
+		if ((System.currentTimeMillis() - exitTime) > 2000){
+			Toast.makeText(MainActivity.this, R.string.toast_exit , Toast.LENGTH_SHORT).show();
+			exitTime = System.currentTimeMillis();
+		}else{
+			finish();
 		}
 	}
 
